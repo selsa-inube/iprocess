eff9e030de74fc5f534b261acc03062505ea797f
carin <ca.rincon97@gmail.com>
2024-06-18 05:12:04 -0500

    feat: husky init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import vitesconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), vitesconfigPaths()],
+  server: {
+    open: true,
+    port: 3000,
+  },
+});
