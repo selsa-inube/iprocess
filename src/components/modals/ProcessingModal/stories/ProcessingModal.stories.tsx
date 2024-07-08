5d7d7413258437058a907ba8a3b8700764855f89
angie pinilla <angiepinillanova@gmail.com>
2024-07-08 16:17:22 -0500

    feat: create the processing modal

---
+++ src/components/modals/ProcessingModal/stories/ProcessingModal.stories.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Meta, StoryFn } from "@storybook/react";
+import { Button } from "@inubekit/button";
+import { ProcessingModal, ProcessingModalProps } from '..';
+
+
+
+const meta: Meta<typeof ProcessingModal> = {
+  title: "modals/ProcessingModal",
+  component: ProcessingModal,
+  decorators: [
+    (Story: StoryFn) => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
+};
+
+const Template: StoryFn<ProcessingModalProps> = (args) => {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setShowModal(true)}>Show Modal</Button>
+      {showModal && (
+        <ProcessingModal {...args} onCloseModal={() => setShowModal(false)} />
+      )}
+    </>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  portalId: "portal",
+};
+
+export default meta;
