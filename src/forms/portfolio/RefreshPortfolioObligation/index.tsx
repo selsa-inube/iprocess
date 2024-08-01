cf4191bf7680bfb507e547cc844481e534deb21f
angie pinilla <angiepinillanova@gmail.com>
2024-08-01 15:10:11 -0500

    feat: create form refresh portfolio obligation

---
+++ src/forms/portfolio/RefreshPortfolioObligation/index.tsx
@@ -0,0 +1,90 @@
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { ChangeEvent, useEffect, useState } from "react";
+
+import { IStartProcessEntry, IEntries, IFieldsEntered } from "@src/forms/types";
+import { RefreshPortfolioObligationUI } from "./interface";
+
+const validationSchema = Yup.object({
+  typeRefresh: Yup.string().required("Este campo no puede estar vacío"),
+  descriptionComplementary: Yup.string(),
+  plannedExecutionDate: Yup.string(),
+});
+
+interface RefreshPortfolioObligationProps {
+  data: IEntries;
+  onStartProcess: () => void;
+  setFieldsEntered: (show: IFieldsEntered) => void;
+}
+
+const initialValues: IStartProcessEntry = {
+  descriptionComplementary: "",
+  typeRefresh: "",
+  plannedExecutionDate: "",
+};
+
+const RefreshPortfolioObligation = (props: RefreshPortfolioObligationProps) => {
+  const { data, setFieldsEntered, onStartProcess } = props;
+
+  const [dynamicValidationSchema, setDynamicValidationSchema] =
+  useState(validationSchema);
+
+const formik = useFormik({
+  initialValues,
+  validationSchema: dynamicValidationSchema,
+  validateOnChange: false,
+  onSubmit: async () => true,
+});
+
+const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  formik.setFieldValue("typeRefresh", event.target.outerText).then(()=>{
+    formik.validateForm().then((errors)=>{
+      formik.setErrors(errors);
+    })
+  });
+};
+
+useEffect(() => {
+  if (
+    data?.plannedAutomaticExecution &&
+    data?.plannedAutomaticExecution === "planned automatic execution"
+  ) {
+    setDynamicValidationSchema(
+      validationSchema.shape({
+        plannedExecutionDate: Yup.string().required(
+          "Este campo es requerido"
+        ),
+      })
+    );
+  }
+}, [data?.plannedAutomaticExecution, setDynamicValidationSchema]);
+
+useEffect(() => {
+  if (formik.values) {
+    setFieldsEntered(formik.values);
+  }
+}, [formik.values, setFieldsEntered]);
+
+const comparisonData = Boolean(
+  (data?.plannedAutomaticExecution &&
+    formik.values.plannedExecutionDate.length > 0 &&
+    formik.values.typeRefresh !== initialValues.typeRefresh &&
+    formik.values.plannedExecutionDate !==
+      initialValues.plannedExecutionDate) ||
+    (!data?.plannedAutomaticExecution &&
+      formik.values.typeRefresh !== initialValues.typeRefresh)
+);
+
+return (
+  <RefreshPortfolioObligationUI
+    formik={formik}
+    data={data}
+    onChange={handleChange}
+    onStartProcess={onStartProcess}
+    comparisonData={comparisonData}
+  />
+);
+};
+
+export type { RefreshPortfolioObligationProps };
+export { RefreshPortfolioObligation };
